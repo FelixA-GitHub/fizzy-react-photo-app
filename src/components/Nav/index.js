83f07838f27dce5f5402789db53877dadd38effa
Your Name <felix.acevedo.tech@gmail.com>
2022-06-10 11:47:28 -0400

    changes to multiple index.js

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import { capitalizeFirstLetter } from '../PhotoList/utils/helpers';
 
 function Nav(props) {
   const {
